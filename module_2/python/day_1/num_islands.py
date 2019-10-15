4fc81d3d5373c4bca6bc419ef8b1587f2837708e
Vincent Russo <vincentrusso1@gmail.com>
2019-10-15 09:27:07 -0400

    Adding num islands python

--- module_2/python/day_1/num_islands.py
+++ module_2/python/day_1/num_islands.py
@@ -12,67 +12,79 @@ import unittest
 from typing import List
 
 
-class Grid:
-    def __init__(self, grid):
-        self.grid = grid
-        self.num_rows = len(grid)
-        self.num_cols = len(grid[0])
+class Graph:
+    def __init__(self, row: int, col: int, graph: List[List[int]]):
+        self._row = row
+        self._col = col
+        self._graph = graph
 
-    def is_valid(self, i, j, visited):
-        return (i >= 0 and i < self.num_rows and
-                j >= 0 and j < self.num_cols and
-                not visited[i][j] and self.grid[i][j])
+    # A function to check if a given cell
+    # (row, col) can be included in DFS
+    def isSafe(self, i: int, j: int, visited: List[List[int]]):
+        # row number is in range, column number
+        # is in range and value is
+        # and not yet visited
+        return (i >= 0 and i < self._row and
+                j >= 0 and j < self._col and
+                not visited[i][j] and self._graph[i][j])
 
-    def dfs(self, i, j, visited):
-
-        # These lists are used to get row and
-        # column numbers of 8 neighbors of a given cell.
-        row_dir = [-1, -1, -1, 0, 0, 1, 1, 1]
-        col_dir = [-1, 0, 1, -1, 1, -1, 0, 1]
-
+    # A utility function to do DFS for a 2D
+    # boolean matrix. It only considers
+    # the 8 neighbours as adjacent vertices
+    def DFS(self, i, j, visited: List[List[int]]):
+        """Depth first search implementation"""
+        # These arrays are used to get row and
+        # column numbers of 8 neighbours
+        # of a given cell
+        rowNbr = [-1, -1, -1,  0, 0,  1, 1, 1]
+        colNbr = [-1,  0,  1, -1, 1, -1, 0, 1]
+          
+        # Mark this cell as visited
         visited[i][j] = True
-
+  
+        # Recur for all connected neighbours
         for k in range(8):
-            if self.is_valid(i + row_dir[k], j + col_dir[k], visited):
-                self.dfs(i + row_dir[k], j + col_dir[k], visited)
+            if self.isSafe(i + rowNbr[k], j + colNbr[k], visited):
+                self.DFS(i + rowNbr[k], j + colNbr[k], visited)
 
-    def num_islands(self):
-        visited = [[False for j in range(self.num_cols)] for i in range(self.num_rows)]
+    # The main function that returns
+    # count of islands in a given boolean
+    # 2D matrix 
+    def countIslands(self):
+        # Make a bool array to mark visited cells. 
+        # Initially all cells are unvisited 
+        visited = [[False for j in range(self._col)]for i in range(self._row)]
 
+        # Initialize count as 0 and travese
+        # through the all cells of
+        # given matrix
         count = 0
-        for i in range(self.num_rows):
-            for j in range(self.num_cols):
-                if visited[i][j] is False and self.grid[i][j] == 1:
-                    self.dfs(i, j, visited)
+        for i in range(self._row):
+            for j in range(self._col):
+                # If a cell with value 1 is not visited yet,
+                # then new island found
+                if visited[i][j] is False and self._graph[i][j] == 1:
+                    # Visit all cells in this island
+                    # and increment island count
+                    self.DFS(i, j, visited)
                     count += 1
         return count
-
-
 
 
 class TestNumIslands(unittest.TestCase):
     """Unit test for Number of Islands."""
 
     def test_1(self):
-        test_1_input = [
-            [1, 1, 1, 1, 0],
-            [1, 1, 0, 1, 0],
-            [1, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0]
-        ]
-        g = Grid(test_1_input)
-        self.assertEqual(g.num_islands(), 1)
+        graph = [[1, 1, 0, 0, 0],
+                 [0, 1, 0, 0, 1],
+                 [1, 0, 0, 1, 1],
+                 [0, 0, 0, 0, 0],
+                 [1, 0, 1, 0, 1]]
 
-    def test_2(self):
-        test_2_input = [
-            ["1", "1", "0", "0", "0"],
-            ["0", "1", "0", "0", "1"],
-            ["1", "0", "0", "1", "1"],
-            ["0", "0", "0", "0", "0"],
-            ["1", "0", "1", "0", "1"]
-        ]
-        g = Grid(test_2_input)
-        self.assertEqual(g.num_islands(), 1)
+        row = len(graph)
+        col = len(graph[0])
+        g = Graph(row, col, graph)
+        self.assertEqual(g.countIslands(), 5)
 
 
 if __name__ == '__main__':
