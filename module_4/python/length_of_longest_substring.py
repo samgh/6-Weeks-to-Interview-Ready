66922eddaeb488f93abf5e41485fe21c9e0e3e76
Vincent Russo <vincentrusso1@gmail.com>
2019-10-28 08:40:35 -0400

    Adding Length of longest substring for Java and for Python

---
+++ module_4/python/length_of_longest_substring.py
@@ -0,0 +1,45 @@
+"""
+Title: Longest substring without repeating characters.
+
+Given a string, find the length of the longest substring without repeating characters.
+
+Execution: python length_of_longest_substring.py
+"""
+import unittest
+
+
+def length_of_longest_substring(s: str):
+    """Given a string, determine the length of the longest substring."""
+    last, res, st = dict(), 0, 0
+    for i, v in enumerate(s):
+        if v not in last or last[v] < st:
+            res = max(res, i - st + 1)
+        else:
+            st = last[v] + 1
+        last[v] = i
+    return res
+
+
+class TestLengthOfLongestSubstring(unittest.TestCase):
+    """Unit tests for length_of_longest_substring."""
+
+    def test_1(self):
+        """Test for string: abcabcbb."""
+        self.assertEqual(length_of_longest_substring("abcabcbb"), 3)
+        print("Explanation: The answer is 'abc', with the length of 3.")
+
+    def test_2(self):
+        """Test for string: bbbbb."""
+        self.assertEqual(length_of_longest_substring("bbbbb"), 1)
+        print("Explanation: The answer is 'b', with the length of 1.")
+
+    def test_3(self):
+        """Test for string: pwwkew."""
+        self.assertEqual(length_of_longest_substring("pwwkew"), 3)
+        print("Explanation: The answer is 'wke', with the length 3.")
+        print("Note that the answer must be a substring, 'pwke' ")
+        print("is a subsequence and not a substring.")
+
+
+if __name__ == '__main__':
+    unittest.main()
