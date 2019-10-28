7d09a9d84a29314613aa0ec995210528d916f8ae
Vincent Russo <vincentrusso1@gmail.com>
2019-10-28 13:07:21 -0400

    Adding in comments for k_closest python

--- module_4/python/k_closest.py
+++ module_4/python/k_closest.py
@@ -1,9 +1,16 @@
 """
-Title:
+Title: K-closest
 
 Problem:
+    We have a list of points on the plane.  Find the K closest points to the
+    origin (0, 0).
 
-Execution: python XXX.py
+    (Here, the distance between two points on a plane is the Euclidean distance.)
+
+    You may return the answer in any order.  The answer is guaranteed to be unique
+    (except for the order that it is in.)
+
+Execution: python k_closest.py
 """
 import random
 import unittest
