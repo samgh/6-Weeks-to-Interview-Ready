c5e9e03bf6ab75479eaa6a03db112a0d242b46ee
Sam Gavis-Hughson <sam@byte-by-byte.com>
2019-10-30 12:38:01 -0400

    Fixed LCS

--- module_1/java/LengthOfLongestSubstring.java
+++ module_1/java/LengthOfLongestSubstring.java
@@ -19,7 +19,7 @@ public class LengthOfLongestSubstring {
         int i = 0;
         for (int j = 0; j < n; j++) {
             i = Math.max(index[s.charAt(j)], i);
-            ans = Math.max(ans, j - 1 + 1);
+            ans = Math.max(ans, j - i + 1);
             index[s.charAt(j)] = j + 1;
         }
         return ans;
