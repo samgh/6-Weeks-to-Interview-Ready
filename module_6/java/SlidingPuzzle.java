d355b643255121b1f8608ac7b780edd7ba871834
Vincent Russo <vincentrusso1@gmail.com>
2019-11-12 11:02:56 -0500

    Changes to sliding puzzle

--- module_6/java/SlidingPuzzle.java
+++ module_6/java/SlidingPuzzle.java
@@ -1,9 +1,20 @@
 /*
- *   Title: 
+ *   Title: Sliding puzzle
  *
  *   Problem:
+ *   On a 2x3 board, there are 5 tiles represented by the integers 1 through 5,
+ *   and an empty square represented by 0.
+
+     A move consists of choosing 0 and a 4-directionally adjacent number and
+     swapping it.
+
+     The state of the board is solved if and only if the board is [[1,2,3],[4,5,0]].
+
+     Given a puzzle board, return the least number of moves required so that the
+     state of the board is solved. If it is impossible for the state of the board to
+     be solved, return -1.
  *
- *   Execution: javac XX && java XX
+ *   Execution: javac SlidingPuzzle.java && java SlidingPuzzle
  */
 import java.util.*;
 
