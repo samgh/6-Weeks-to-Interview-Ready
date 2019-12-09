be34449ac83353f9545c0567ae39f94272136ed2
Vincent Russo <vincentrusso1@gmail.com>
2019-12-08 21:02:00 -0500

    Adding StackFromQueue.java

--- module_1/java/StackFromQueue.java
+++ module_1/java/StackFromQueue.java
@@ -6,37 +6,35 @@
  *
  *   Execution: javac StackFromQueue.java && java StackFromQueue
  */
-
-import java.util.Queue;
-import java.util.LinkedList;
-
-public class StackFromQueue {
-
-    public static class Stack {
-        private Queue<Integer> queue = new LinkedList<>();
-
-        Stack() {
-        }
-
-        public void push(int x) {
-            queue.add(x);
-            for (int i = 1; i < queue.size(); i++) {
-                queue.add(queue.remove());
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-
-        Stack stack = new Stack();
-        stack.push(5);
-
-        //stack = new StackFromQueue();
-
-        //assert template("abcabcbb") == 3;
-        System.out.println("Explanation: ");
-
-        System.out.println("Passed all test cases");
-    }
+import java.util.*; 
+  
+class StackFromQueue {  
+    static Queue<Integer> queue = new LinkedList<Integer>();
     
-}
+    public static void push(int value) {
+        int qSize = queue.size();
+        queue.add(value);
+
+        for (int i = 0; i < qSize; i++) {
+            queue.add(queue.remove());
+        }
+    }
+
+    public static void pop() {
+        System.out.println("Element removed from stack is:" + queue.remove());
+    }
+      
+    // Driver code  
+    public static void main(String[] args) {  
+        StackFromQueue stackFromQueue = new StackFromQueue();
+        stackFromQueue.push(1);
+        stackFromQueue.push(2);
+        stackFromQueue.push(3);
+        stackFromQueue.push(4);
+ 
+        System.out.println(stackFromQueue.queue);
+        stackFromQueue.pop();
+        System.out.println(stackFromQueue.queue);
+    }  
+}  
+
