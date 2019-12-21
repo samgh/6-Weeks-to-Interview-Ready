affc7fc2c8fc0917b48993bc2afc4268e118ea30
Vincent Russo <vincentrusso1@gmail.com>
2019-12-21 12:08:18 -0500

    Adding python set nth bit

---
+++ quickstart_guides/bit_manipulation/python/set_nth_bit.py
@@ -0,0 +1,38 @@
+"""
+Title: Set n^th bit
+
+Problem: 
+    Write a program  that takes an integer and
+    sets the n-th bit in the binary representation of
+    that integer
+    For instance, the binary representation of 6 is:
+        110
+    The least significant bit is the bit on the far right
+    of the binary representation and the most significant
+    bit is the bit on the far left. We order the bits as
+    b2, b1, b0
+    1   1   0
+    For our function, if we set the 0th bit, we should obtain
+    the binary representation:
+        1 1 1
+
+Execution: python set_nth_bit.py
+"""
+from typing import List
+import unittest
+
+
+def set_nth_bit(x: int, n: int) -> int:
+    return x | 1 << n
+
+
+class TestSetNthBit(unittest.TestCase):
+    """Unit test for set_nth_bit."""
+
+    def test_1(self):
+        self.assertEqual(set_nth_bit(6, 0), 7)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
