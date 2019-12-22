1bcc99c2fc11f9d439437ef418d1db0285f75bff
Vincent Russo <vincentrusso1@gmail.com>
2019-12-21 21:03:00 -0500

    Adding to sort string

---
+++ quickstart_guides/arrays_strings/python/sort_string.py
@@ -0,0 +1,27 @@
+"""
+Title: Sort string
+
+Problem:
+    Given a string of lowercase characters from ‘a’ – ‘z’. We need to write a
+    program to print the characters of this string in sorted order.
+
+Execution: python sort_string.py
+"""
+import unittest
+
+
+def sort_string(s: str) -> str:
+    return ''.join(sorted(s))
+
+
+class TestIsAnagram(unittest.TestCase):
+    """Unit test for sort_string."""
+
+    def test_1(self):
+        s = "bbccdefbbaa"
+        self.assertEqual(sort_string(s), "aabbbbccdef")
+
+
+if __name__ == '__main__':
+    unittest.main()
+
