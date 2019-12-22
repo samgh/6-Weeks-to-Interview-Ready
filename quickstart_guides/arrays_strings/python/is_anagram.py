e9bb9a76646ea7c1be3d5ce3a2932cb7eb810111
Vincent Russo <vincentrusso1@gmail.com>
2019-12-21 20:32:04 -0500

    Adding to is anagram

--- quickstart_guides/arrays_strings/python/is_anagram.py
+++ quickstart_guides/arrays_strings/python/is_anagram.py
@@ -7,16 +7,40 @@ Problem:
 
 Execution: python is_anagram.py
 """
+from collections import defaultdict
 import unittest
 
 
 def is_anagram_sorting(s: str, t: str) -> bool:
-    """Check if string s is anagram of string t."""
+    """Check if string s is anagram of string t using sorting."""
     if len(s) != len(t):
         return False
     s = sorted(s)
     t = sorted(t)
     return s == t
+
+
+def is_anagram_hash_table(s: str, t: str) -> bool:
+    """Check if string s is anagram of string t using hash table."""
+    ht = dict()
+
+    if len(s) != len(t):
+        return False
+
+    for i in s:
+        if i in ht:
+            ht[i] += 1
+        else:
+            ht[i] = 1
+    for i in t:
+        if i in ht:
+            ht[i] -= 1
+        else:
+            ht[i] = 1
+    for i in ht:
+        if ht[i] != 0:
+            return False
+    return True
 
 
 class TestIsAnagram(unittest.TestCase):
@@ -26,11 +50,13 @@ class TestIsAnagram(unittest.TestCase):
         s = "anagram"
         t = "nagaram"
         self.assertEqual(is_anagram_sorting(s, t), True)
+        self.assertEqual(is_anagram_hash_table(s, t), True)
 
     def test_2(self):
         s = "rat"
         t = "car"
         self.assertEqual(is_anagram_sorting(s, t), False)
+        self.assertEqual(is_anagram_hash_table(s, t), False)
 
 
 if __name__ == '__main__':
