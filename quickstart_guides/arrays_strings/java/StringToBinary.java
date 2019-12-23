4f16699b6884a37a1670404afa5422ce239780de
Vincent Russo <vincentrusso1@gmail.com>
2019-12-23 12:45:05 -0500

    Adding StringToBinary java

---
+++ quickstart_guides/arrays_strings/java/StringToBinary.java
@@ -0,0 +1,63 @@
+/*
+ *   Title: Convert string to binary sequence
+ *   Problem:
+ *   Given a string of character the task is to convert each character of a
+ *   string into the equivalent binary number.
+ *
+ *   Execution: javac StringToBinary.java && java StringToBinary
+ */
+import java.util.*;
+
+
+public class StringToBinary {
+        // utility function
+    static String stringToBinary(String s) {
+        int n = s.length();
+
+        String bin = "";
+        for (int i = 0; i < n; i++) {
+            // convert each char to
+            // ASCII value
+            int val = Integer.valueOf(s.charAt(i));
+
+            // Convert ASCII value to binary
+            bin = "";
+            while (val > 0) {
+                if (val % 2 == 1) {
+                    bin += '1';
+                }
+                else {
+                    bin += '0';
+                }
+                val /= 2;
+            }
+            bin = reverse(bin);
+            System.out.print(bin + " ");
+        }
+        return bin;
+    }
+
+    static String reverse(String input)
+    {
+        char[] a = input.toCharArray();
+        int l, r = 0;
+        r = a.length - 1;
+
+        for (l = 0; l < r; l++, r--) {
+            // Swap values of l and r
+            char temp = a[l];
+            a[l] = a[r];
+            a[r] = temp;
+        }
+        return String.valueOf(a);
+    } 
+
+    // Sample test cases
+    public static void main(String[] args) {
+        String s = "BbB";
+        assert stringToBinary(s) == "1000010 1100010 1000010";
+
+        System.out.println("Passed all test cases");
+    }
+    
+}
