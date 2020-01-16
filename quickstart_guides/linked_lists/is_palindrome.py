02ad13eac00b2de5c751ba316bc79f51dd7d20d1
Vincent Russo <vincentrusso1@gmail.com>
2020-01-16 12:47:35 -0500

    Adding is palindrome python linked list

---
+++ quickstart_guides/linked_lists/is_palindrome.py
@@ -0,0 +1,10 @@
+"""
+Title: Linked list is palindrome
+
+Problem:
+    Check if linked list has nodes that consist of palindrome.
+
+Execution: python is_palindrome.py
+"""
+from typing import List
+import unittest
