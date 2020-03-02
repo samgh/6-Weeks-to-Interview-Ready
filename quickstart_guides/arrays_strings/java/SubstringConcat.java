22c3f47cced8959d166f6588dd4ba48a53f875b7
Vincent Russo <vincentrusso1@gmail.com>
2020-03-02 17:05:42 -0500

    Updating arrays_and_strings with complete java/python exercises

---
+++ quickstart_guides/arrays_strings/java/SubstringConcat.java
@@ -0,0 +1,76 @@
+/*
+ *   Title: Substring with Concatenation of All Words
+ *   Problem:
+ *
+        You are given a string, s, and a list of words, words, that are all of the same
+        length. Find all starting indices of substring(s) in s that is a concatenation
+        of each word in words exactly once and without any intervening characters.
+
+ *   Execution: javac SubstringConcat.java && java SubstringConcat
+ */
+import java.util.*;
+
+
+public class SubstringConcat {
+    static List<Integer> substringConcat(String s, String[] words) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if (s == null || s.length() == 0 || words == null || words.length == 0)
+            return res;
+        int wordLength = words[0].length();
+        HashMap<String, Integer> needs = new HashMap<String, Integer>();
+        for (String word : words) {
+            needs.put(word, needs.getOrDefault(word, 0) + 1);
+        }
+
+        for (int i = 0; i < wordLength; i++) {
+            int left = i, right = i, match = 0;
+            HashMap<String, Integer> window = new HashMap<>();
+
+            while (right < s.length() - wordLength + 1) {
+                String s1 = s.substring(right, right + wordLength);
+                if (needs.containsKey(s1)) {
+                    window.put(s1, window.getOrDefault(s1, 0) + 1);
+                    if (window.get(s1) == needs.get(s1)) {
+                        match++;
+                    }
+                }
+                right += wordLength;
+                while (needs.size() == match) {
+                    if (right - left == wordLength * words.length) {
+                        res.add(left);
+                    }
+                    String s2 = s.substring(left, left + wordLength);
+                    if (needs.containsKey(s2)) {
+                        window.put(s2, window.get(s2) - 1);
+                        if (window.get(s2) < needs.get(s2)) {
+                            match--;
+                        }
+                    }
+                    left += wordLength;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        String s1 = "barfoothefoobarman";
+        String[] words1 = {"foo", "bar"};
+
+        List<Integer> expected_res1 = new ArrayList<Integer>(){{
+                        add(0);
+                        add(9);
+                          }};
+        assert substringConcat(s1, words1).equals(expected_res1);
+
+        String s2 = "wordgoodgoodgoodbestword";
+        String[] words2 = {"word","good","best","word"};
+        List<Integer> expected_res2 = new ArrayList<Integer>(){{
+                          }};
+        assert substringConcat(s2, words2).equals(expected_res1);
+
+        System.out.println("Passed all test cases");
+    }
+    
+}
---
+++ quickstart_guides/bit_manipulation/README.txt
@@ -0,0 +1,35 @@
+Determine whether a number is even or odd (LucidProgramming Video)
+Java: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/bit_manipulation/java/IsEvenOdd.java
+Python: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/bit_manipulation/python/is_even_odd.py
+
+Set the nth bit of a number (LucidProgramming Video)
+Java: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/bit_manipulation/java/SetNthBit.java
+Python: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/bit_manipulation/python/set_nth_bit.py
+
+Determine whether a number is a power of 2 (Leetcode)
+Java: 
+Python: 
+
+Determine whether a number is a power of 4 (Leetcode)
+Java: 
+Python: 
+
+Reverse the bits of an unsigned integer (Leetcode)
+Java: 
+Python: 
+
+Count the number of 1 bits in a number (Leetcode)
+Java: 
+Python: 
+
+Find the Hamming Distance between two numbers (Leetcode)
+Java: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/bit_manipulation/java/HammingDistance.java
+Python: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/bit_manipulation/python/hamming_distance.py
+
+Sum 2 Numbers (Leetcode)
+Java: 
+Python: 
+
+Missing number (Leetcode)
+Java: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/bit_manipulation/java/MissingNumber.java
+Python: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/bit_manipulation/python/missing_number.py
