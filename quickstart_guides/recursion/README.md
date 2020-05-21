758b0544505751d6bd855ff44f41a21a46106cd2
Vincent Russo <vincentrusso1@gmail.com>
2020-05-21 14:31:10 -0400

    Adding IsPalindrome.java for recursion quickstart

--- quickstart_guides/recursion/README.md
+++ quickstart_guides/recursion/README.md
@@ -17,7 +17,7 @@ Java: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart
 Python: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/recursion/python/XX.py
 
 ## Is a string a palindrome? (Geeks for Geeks)
-Java: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/recursion/java/XX.java
+Java: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/recursion/java/IsPalindrome.java
 Python: https://github.com/samgh/6-Weeks-to-Interview-Ready/blob/master/quickstart_guides/recursion/python/is_palindrome.py
 
 ## Stair steps (Leetcode)
---
+++ quickstart_guides/recursion/java/IsPalindrome.java
@@ -0,0 +1,36 @@
+/*
+ *   Title: Is palindrome
+ *
+ * Problem:
+    Given a string, determine if it is a palindrome, considering only
+    alphanumeric characters and ignoring cases.
+
+    Note: For the purpose of this problem, we define empty string as valid
+    palindrome.
+ *
+ *   Execution: javac IsPalindrome.java && java IsPalindrome
+ */
+import java.util.Arrays;
+
+
+public class IsPalindrome {
+    public static boolean isPalindrome(String s) {
+        if((s == null) || (s.length() == 0)) {
+            return true;
+        }
+
+        s = String.join("",Arrays.asList(s.split("\\W+")));
+        s = s.toLowerCase();
+
+        String snew = new StringBuilder(s).reverse().toString();
+        return s.equals(snew);
+    }
+
+    public static void main(String[] args) {
+        assert isPalindrome("A man, a plan, a canal: Panama") == true;
+        assert isPalindrome("race a car") == false;
+
+        System.out.println("Passed all test cases");
+    }
+    
+}
